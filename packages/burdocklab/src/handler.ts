c0dec13feb432373b8ea2a9ba36e7c43a981ba03
Dylan Lukes <lukes.dylan@gmail.com>
2019-08-26 13:23:18 -0700

    back to running

--- packages/burdocklab/src/handler.ts
+++ packages/burdocklab/src/handler.ts
@@ -1,11 +1,22 @@
-import {IDisposable} from "@phosphor/disposable";
-import {ISignal, Signal} from "@phosphor/signaling";
+import { IDisposable } from "@phosphor/disposable";
+import {
+    ISignal,
+    Signal
+} from "@phosphor/signaling";
 
-import {IBurdockInspector} from "./tokens";
+import { IBurdockInspector } from "./tokens";
+import IBurdockInspectorUpdate = IBurdockInspector.IBurdockInspectorUpdate;
 
 export class BurdockInspectionHandler implements IDisposable, IBurdockInspector.IBurdockInspectable {
-    private _disposed: Signal<this, void> = new Signal<this, void>(this);
+    private _disposed: Signal<this, void>
+        = new Signal<this, void>(this);
+
     private _isDisposed: boolean = false;
+
+    private _inspected: Signal<any, IBurdockInspectorUpdate>
+        = new Signal<any, IBurdockInspectorUpdate>(this);
+
+    private _standby: boolean;
 
     get disposed(): ISignal<this, void> {
         return this._disposed;
@@ -20,5 +31,17 @@ export class BurdockInspectionHandler implements IDisposable, IBurdockInspector.
         this._isDisposed = true;
         this._disposed.emit(void 0);
         Signal.clearData(this);
+    }
+
+    get inspected(): ISignal<any, IBurdockInspectorUpdate> {
+        return this._inspected;
+    }
+
+    get standby(): boolean {
+        return this._standby;
+    }
+
+    set standby(value: boolean) {
+        this._standby = value;
     }
 }
